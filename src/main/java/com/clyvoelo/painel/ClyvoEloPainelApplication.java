eb52046f95c45ca84ad7ab18bffac07dad74998e
GabrielCabralmm <gabriel.cmmariano@gmail.com>
2026-09-13 13:32:34 -0300

    Configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/clyvoelo/painel/ClyvoEloPainelApplication.java
@@ -0,0 +1,11 @@
+package com.clyvoelo.painel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClyvoEloPainelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClyvoEloPainelApplication.class, args);
+    }
+}
